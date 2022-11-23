87012ec560a23b4de9fceca491dc42a364ef801a
Funkysys <aadelbos@gmail.com>
2022-11-23 18:04:40 +0100

    add Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,7 +13,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <iframe className={styles.video} src="https://www.youtube.com/embed/bUEwCbePivY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <button className={styles.button}>Buy Tickets</button>
+        <button className={styles.button}>
+          <Link href={"https://www.eventbrite.com/e/la-suspendida-a-jazz-metal-opera-tickets-470614228827"}>
+            Buy Tickets
+          </Link>
+        </button>
       </main>
 
 
